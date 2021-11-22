47493b27c5e6739a8e55ab61b728cbefe3d802a1
francistov98 <60896967+francistov98@users.noreply.github.com>
2021-11-21 22:26:18 -0600

    Create desarrollo.php

---
+++ biblioteca-virtual/desarrollo.php
@@ -0,0 +1,9 @@
+<?php include("template/cabecera.php");?>
+
+<div class="jumbotron">
+    <h1 class="display-3">Prueba de ventana</h1>
+    <p class="lead">Hola mundo</p>
+    <hr class="my-2">
+</div>
+
+<?php include("template/pie.php"); ?>
