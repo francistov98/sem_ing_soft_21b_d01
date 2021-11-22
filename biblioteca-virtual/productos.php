ab214e8bbbdfeebac7f2ea1ce978ca30b8396b7b
francistov98 <60896967+francistov98@users.noreply.github.com>
2021-11-21 22:23:44 -0600

    Create productos.php

---
+++ biblioteca-virtual/productos.php
@@ -0,0 +1,64 @@
+<?php include("template/cabecera.php");?>
+
+<div class="col-md-3">
+
+<div class="card">
+    
+    <img class="card-img-top" src="./imagenes/lv1.png" alt="">
+
+    <div class="card-body">
+        <h4 class="card-title">Libro PHP Lv1</h4>
+        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más </a>
+    </div>
+
+</div>
+    
+</div>
+
+<div class="col-md-3">
+
+<div class="card">
+    
+    <img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar1.png" alt="">
+
+    <div class="card-body">
+        <h4 class="card-title">Libro PHP Lv2</h4>
+        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más </a>
+    </div>
+
+</div>
+    
+</div>
+
+<div class="col-md-3">
+
+<div class="card">
+    
+    <img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar1.png" alt="">
+
+    <div class="card-body">
+        <h4 class="card-title">Libro PHP Lv3</h4>
+        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más </a>
+    </div>
+
+</div>
+    
+</div>
+
+<div class="col-md-3">
+
+<div class="card">
+    
+    <img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar1.png" alt="">
+
+    <div class="card-body">
+        <h4 class="card-title">Libro PHP Lv4</h4>
+        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más </a>
+    </div>
+
+</div>
+    
+</div>
+
+
+<?php include("template/pie.php"); ?>
